5dc878654ebf57d8acc027f72944a30c57bf46f0
Damilola Ayeni <Oluwadamilolaayeni321@gmail.com>
2025-06-22 13:56:53 +0100

    Update 8-script.js

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+  console.log("Script is loaded ✅"); // Debug
   const hamburger = document.getElementById('hamburger');
   const navLinks = document.getElementById('navLinks');
 
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+    console.log("Hamburger clicked! 🍔");
   });
 });
